137c72e15b463eca5c192ded8465a9a835b57403
doringraf <doringraf@gmail.com>
2020-02-29 14:57:32 -0500

    1.1

--- src/test/Login.java
+++ src/test/Login.java
@@ -4,10 +4,7 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("user logged in");
         int count =1;
-        
-        while(count!=5){
-                  System.out.println("it's conting to "+count);
-        }
+
 
     }
 }

a1a2d270c22f24b3de7b44b00d70ac32bb67b53d
pshilin <58673233+pshilin@users.noreply.github.com>
2020-02-29 15:48:14 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -3,5 +3,8 @@ package test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Reservation Canceling");
+        
+        System.out.println("good bye"); //from GH
+        
     }
 }
